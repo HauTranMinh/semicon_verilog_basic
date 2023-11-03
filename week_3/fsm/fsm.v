65c1d590a9e91740c68501d479626a5396ce4912
HauTranMinh <tranminhhau.work@gmail.com>
2023-11-03 14:24:46 +0700

    add TB fir fsm but still have issues with output

--- week_3/fsm/fsm.v
+++ week_3/fsm/fsm.v
@@ -1,71 +1,72 @@
-module fsm(data_in, signal_out, clk, rst_n);
+module fsm(data_in, signal_out, clk, rst_n, state, next_state);
 
 	input data_in, clk, rst_n;
-	output signal_out;
+	output signal_out, state, next_state;
 
 	reg [1:0] state, next_state;
+	reg signal;
 	
-
 	always @(posedge clk or negedge rst_n) begin
 		case (state)
 			// state A: 00 
 			2'b00: 
 				if (data_in) begin
-					state <= 2'b01;
-					signal_out <= 1'b0;
+					next_state <= 2'b01;
+					signal <= 1'b0;
 				end
 				else begin
-					state <= 2'b00;
-					signal_out <= 1'b0;
+					next_state <= 2'b00;
+					signal <= 1'b0;
 				end
 
 			// state B: 01 
 			2'b01: 
 				if (data_in) begin
-					state <= 2'b10;
-					signal_out <= 1'b0;
+					next_state <= 2'b10;
+					signal <= 1'b0;
 				end
 				else begin
-					state <= 2'b00;
-					signal_out <= 1'b0;
+					next_state <= 2'b00;
+					signal <= 1'b0;
 				end
 
 			// state C: 10 
 			2'b10: 
 				if (data_in) begin
-					state <= 2'b10;
-					signal_out <= 1'b0;
+					next_state <= 2'b10;
+					signal <= 1'b0;
 				end
 				else begin
-					state <= 2'b11;
-					signal_out <= 1'b0;
+					next_state <= 2'b11;
+					signal <= 1'b0;
 				end
 
 			// state D: 11
 			2'b11: 
 				if (data_in) begin
-					state <= 2'b01;
-					signal_out <= 1'b1;
+					next_state <= 2'b01;
+					signal <= 1'b1;
 				end
 				else begin
-					state <= 2'b00;
-					signal_out <= 1'b0;
+					next_state <= 2'b00;
+					signal	 <= 1'b0;
 				end
-			default:
-					state <= 0;
-					signal_out <= 0;
+			default: state <= 2'b00;
 		endcase
-
 	end
-	
+
+
+	// Flips_flops
 	always @(posedge clk or negedge rst_n) begin
 		if (~rst_n) begin
-			// reset
-			state <= 2'b00;
+			// reset 
+			next_state <= 2'b00;
 		end
 		else begin
 			state <= next_state;
 		end
 	end
-	
+
+	assign signal_out = signal;
+
 endmodule 
---
+++ week_3/fsm/fsm_tb.v
@@ -0,0 +1,72 @@
+module fsm_tb;
+  reg clk;
+  reg rst_n;
+  wire [1:0] state, next_state;
+  reg data_in;
+  wire signal_out;
+
+
+  // Instantiate the FSM module
+  fsm dut (
+    .data_in(data_in),
+    .signal_out(signal_out),
+    .clk(clk),
+    .rst_n(rst_n),
+    .state(state),
+    .next_state(next_state)
+  );
+
+  // Clock generation
+  always begin
+    #5 clk = ~clk;
+  end
+
+  // Initial values
+  initial begin
+    clk = 0;
+    rst_n = 0;
+    //state = 2'b00;
+    //data_in = 0;
+
+    // Apply a reset and allow time for it to settle
+    rst_n = 1;
+    #5 rst_n = 0;
+    #5 rst_n = 1;
+
+    // Test sequence: "10011001111010"
+    #5 data_in = 1;
+    #5 data_in = 0;
+    #5 data_in = 0;
+    #5 data_in = 1;
+    #5 data_in = 1;
+    #5 data_in = 0;
+    #5 data_in = 0;
+    #5 data_in = 1;
+    #5 data_in = 1;
+    #5 data_in = 1;
+    #5 data_in = 0;
+    #5 data_in = 1;
+    #5 data_in = 0;
+    // Add more delays or additional data_in values if needed
+    #20;
+    // Finish the simulation
+    $finish;
+  end
+
+  // Display the state
+  always @(posedge clk) begin
+    $display("State: %b, Signal Out: %b", state, signal_out);
+  end
+
+endmodule
+
+// Run the simulation
+// initial begin
+//   $dumpfile("fsm_tb.vcd");
+//   $dumpvars(0, fsm_tb);
+//   $display("Starting FSM testbench...");
+//   $monitor("Time: %t, State: %b, Signal Out: %b", $time, state, signal_out);
+//   $timeformat(-9, 1, " ns", 10);
+//   $start = 0;
+//   #1000 $stop;
+// end
